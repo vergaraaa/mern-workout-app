eb7031e3c6edd52060936e1f15f6a19e97a7f97a
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-10 11:37:29 +0200

    login and signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -11,9 +13,17 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
+            <Route
               path="/"
               element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
             />
           </Routes>
         </div>
