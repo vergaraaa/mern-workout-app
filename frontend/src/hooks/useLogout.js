59d67ea07ee909c1268ecd149b483a39720314fb
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-10 20:33:26 +0200

    get user workouts and clear workouts

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,11 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch } = useWorkoutsContext()
 
     const logout = () => {
         // remove user from storage
         localStorage.removeItem('user')
+
+        // remove workouts from context
+        workoutsDispatch({ type: 'SET_WORKOUTS', payload: null })
 
         // dispatch logout action
         dispatch({ type: 'LOGOUT' })
