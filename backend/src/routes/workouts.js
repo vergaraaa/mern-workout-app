aa67ef843f6435963eb05d562ef56bbaadc5e7e2
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-10 19:51:56 +0200

    protecting api workout routes

--- backend/src/routes/workouts.js
+++ backend/src/routes/workouts.js
@@ -6,8 +6,12 @@ const {
   deleteWorkout,
   updateWorkout
 } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
+// require auth for all workout routes
 const router = express.Router()
+
+router.use(requireAuth)
 
 // GET all workouts
 router.get('/', getWorkouts)
